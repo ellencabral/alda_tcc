247e94102dadf7e91497b5f089a3a44e3a917f6f
Ellen <ellencabral2098@gmail.com>
2024-03-23 15:09:02 -0300

    refactor: change list style

--- resources/views/commissions/index.blade.php
+++ resources/views/commissions/index.blade.php
@@ -1,12 +1,12 @@
 <x-layout title="Commissions">
     <ul class="list-group">
         @foreach($commissions as $commission)
-            <li class="list-group-item d-flex justify-content-between align-items-center">
+            <li class="list-group-item">
                 Descrição: {{ $commission->description }}
             </li>
-            <ul>
+            <ul class="list-group-item">Produtos:
                 @foreach($commission->products as $product)
-                    <li>{{ $product->name }}</li>
+                    <li class="list-group-item">{{ $product->name }}</li>
                 @endforeach
             </ul>
 
