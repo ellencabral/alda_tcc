c5732bca1c9b6ad4d56f9fd5eb86bf9986f95e23
Ellen <ellencabral2098@gmail.com>
2024-04-03 18:04:42 -0300

    :bug: fix: set user timestamps to false

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone', 11)->unique()->nullable();
-            $table->string('password', 50);
+            $table->string('password', 255);
             $table->binary('image')->nullable();
             $table->enum('type', ['user','admin'])->default('user');
             $table->boolean('is_active')->default(1);
