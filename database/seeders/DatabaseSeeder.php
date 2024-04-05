24259ccdff33fbf2ba06e7b71a88159f83843710
Ellen <ellencabral2098@gmail.com>
2024-04-04 22:35:08 -0300

    :recycle: refactor: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Status;
+use App\Models\User;
+use App\Models\UserType;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        UserType::factory(2)
+            ->sequence(
+                ['type' => 'user'],
+                ['type' => 'admin'],
+            )
+            ->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(4)->create();
+
+        Status::factory(6)
+            ->sequence(
+                ['description' => 'Pedido Efetuado'],
+                ['description' => 'Aguardando Pagamento'],
+                ['description' => 'Em Preparação'],
+                ['description' => 'Pedido Enviado'],
+                ['description' => 'Em Transporte'],
+                ['description' => 'Pedido Entregue'],
+            )
+            ->create();
     }
 }
